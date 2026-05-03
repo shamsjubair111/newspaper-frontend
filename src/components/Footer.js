00697e5e7d942ee41f579bf129e31669b7cefdb7
shamsjubair111 <shamsjubair.sj@gmail.com>
2026-05-03 09:27:12 +0600

    footer issue fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -49,27 +49,13 @@ const Footer = () => {
               </div>
             </div>
 
-            {/* Column 1 */}
-            <div className="footer-col">
-              <h4 className="footer-col-title">{t('footerCategories')}</h4>
-              <ul className="footer-links">
-                <li><Link to="/প্রবন্ধ">{t('footerLinkProbandhya')}</Link></li>
-                <li><Link to="/%E0%A6%AC%E0%A6%BF%E0%A6%B6%E0%A7%87%E0%A6%B7%20%E0%A6%B8%E0%A7%8D%E0%A6%AC%E0%A6%BE%E0%A6%95%E0%A7%8D%E0%A6%B7%E0%A6%BE%E0%A7%8E%E0%A6%95%E0%A6%BE%E0%A6%B0">{t('footerLinkInterview')}</Link></li>
-                <li><Link to="/%E0%A6%85%E0%A6%A8%E0%A7%81%E0%A6%AC%E0%A6%BE%E0%A6%A6">{t('footerLinkTranslation')}</Link></li>
-                <li><Link to="/%E0%A6%AC%E0%A7%81%E0%A6%95%20%E0%A6%B0%E0%A6%BF%E0%A6%AD%E0%A6%BF%E0%A6%89">{t('footerLinkBookReview')}</Link></li>
-                <li><Link to="/%E0%A6%B0%E0%A6%BE%E0%A6%9C%E0%A6%A8%E0%A7%80%E0%A6%A4%E0%A6%BF">{t('footerLinkNibandha')}</Link></li>
-              </ul>
-            </div>
-
             {/* Column 2 */}
             <div className="footer-col">
               <h4 className="footer-col-title">{t('footerInfo')}</h4>
               <ul className="footer-links">
-                <li><Link to="/archive">{t('footerLinkArchive')}</Link></li>
                 <li><Link to="/about">{t('footerLinkAbout')}</Link></li>
                 <li><Link to="/submission">{t('footerLinkSubmission')}</Link></li>
-                <li><Link to="/contact">{t('footerLinkContact')}</Link></li>
-                <li><Link to="/privacy">{t('footerLinkPrivacy')}</Link></li>
+               
               </ul>
             </div>
 
